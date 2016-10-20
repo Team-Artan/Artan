48887f63b34c1f5515054ccdded345e41e65aefc
DaeHyun Kang <limitedcopy@gmail.com>
2016-10-20 23:02:40 +0900

    Unity project commit

---
+++ Assets/Scripts/BombEffect.cs
@@ -0,0 +1,38 @@
+﻿    using UnityEngine;
+    using System.Collections;
+
+public class BombEffect : MonoBehaviour
+{
+
+    public GameObject effect;
+    public GameObject Location;
+    GameObject temp;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && temp == null)
+        {
+            temp = GameObject.Instantiate(effect, Location.transform.position, Location.transform.rotation) as GameObject;
+        }
+        if (temp != null)
+        {
+            if(temp.GetComponent<ParticleSystem>().isPlaying == false)
+            {
+                Debug.Log("stop");
+                Destroy(temp);
+            }
+        }
+  
+
+        
+    }
+}

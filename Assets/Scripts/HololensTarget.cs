a86d197229ac7348df6cc1dd81a78ed6b7275feb
Seung Ho Jang <shjang1992@gmail.com>
2016-11-05 03:51:19 +0900

    HoloLensTarget added

---
+++ Assets/Scripts/HololensTarget.cs
@@ -0,0 +1,67 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class HololensTarget : MonoBehaviour {
+    private GestureManager gestureManager;
+
+    public bool IsGazing { get; private set; }
+    public bool IsPressed { get; private set; }
+    public bool IsManipulating { get; private set; }
+    public bool IsSelected { get; private set; }
+
+    private void Start()
+    {
+        IsGazing = false;
+        IsPressed = false;
+        IsManipulating = false;
+        IsSelected = false;
+
+        gestureManager = GestureManager.Instance;
+        gestureManager.OnManipulationStarted += BeginManipulation;
+        gestureManager.OnManipulationCompleted += EndManipulation;
+        gestureManager.OnManipulationCanceled += EndManipulation;
+    }
+
+    private void Update()
+    {
+        if (IsSelected == true) {
+            IsSelected = false;
+        }
+    }
+
+    private void OnGazeEnter()
+    {
+        IsGazing = true;
+    }
+
+    private void OnGazeLeave()
+    {
+        IsGazing = false;
+    }
+
+    private void OnPressed()
+    {
+        IsPressed = true;
+    }
+
+    private void OnReleased()
+    {
+        IsPressed = false;
+    }
+
+    private void OnSelect()
+    {
+        IsSelected = true;
+    }
+
+    private void BeginManipulation(InteractionSourceKind sourceKind)
+    {
+        IsManipulating = true;
+    }
+
+    private void EndManipulation(InteractionSourceKind sourceKind)
+    {
+        IsManipulating = false;
+    }
+}

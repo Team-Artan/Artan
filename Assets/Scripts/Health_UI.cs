1e0288e90d707398a8ffc4079caa95e641a0cccd
DaeHyun Kang <limitedcopy@gmail.com>
2016-11-05 15:27:53 +0900

    Add files via upload

--- Assets/Scripts/Health_UI.cs
+++ Assets/Scripts/Health_UI.cs
@@ -8,6 +8,7 @@ public class Health_UI : MonoBehaviour {
     GameObject HPSurface;
     GameObject HP_Bar;
     float HP;
+
 	// Use this for initialization
 	void Start () {
         HPSurface = GameObject.Find("HP_Surface");
@@ -18,24 +19,30 @@ public class Health_UI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position = new Vector3(targetTank.transform.position.x, targetTank.transform.position.y+4f, targetTank.transform.position.z);
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    GetDamaged(20);
-        //}
-        //if (Input.GetKeyDown(KeyCode.H))
-        //{
-        //    GetHeal(20);
-        //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            GetDamaged(20);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GetHeal(20);
+        }
     }
 
     public void GetDamaged(float damage)
     {
+        StopAllCoroutines();
+        StartCoroutine(vibrate());
+        StartCoroutine(col_dam());
+        
         if (HP_Bar.GetComponent<Image>().fillAmount > 0)
         {
             if (HP_Bar.GetComponent<Image>().fillAmount - damage / fullHP >= 0)
             {
                 //HP_Bar.GetComponent<Image>().fillAmount -= damage / fullHP;
                 StartCoroutine(hp_damp(HP_Bar.GetComponent<Image>().fillAmount - damage / fullHP));
+                
             }
             else
             {
@@ -57,6 +64,28 @@ public class Health_UI : MonoBehaviour {
                 StartCoroutine(hp_damp(1));
             }
         }
+    }
+
+    IEnumerator vibrate()
+    {
+        
+        for (int deg = 0; deg <= 1080; deg+=60)
+        {
+            transform.position += new Vector3(Mathf.Sin(deg * Mathf.Deg2Rad), 0, 0);
+            deg++;
+            yield return null;
+        }
+    }
+    IEnumerator col_dam()
+    {
+        HPSurface.GetComponent<Image>().color = new Color(255f, 0, 0);
+        while (HPSurface.GetComponent<Image>().color.g < 255)
+        {
+            HPSurface.GetComponent<Image>().color += new Color(0, 0.05f, 0.05f);
+            yield return null;
+        }
+
+        
     }
 
     IEnumerator hp_damp(float change)

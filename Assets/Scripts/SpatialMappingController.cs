789fbf52ff9c8dc5282f38c84dc8ee78e567c2ea
Seung Ho Jang <shjang1992@gmail.com>
2016-11-05 21:41:48 +0900

    Spatial mapping controller added

---
+++ Assets/Scripts/SpatialMappingController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity;
+
+[RequireComponent(typeof(SpatialMappingManager))]
+public class SpatialMappingController : MonoBehaviour {
+    private SpatialMappingManager smm;
+
+    private void Start()
+    {
+        smm = GetComponent<SpatialMappingManager>();
+    }
+}

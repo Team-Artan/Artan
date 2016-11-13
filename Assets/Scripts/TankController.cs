36b2f1b6b91068614acade6f76b41ade7d1bf956
Seung Ho Jang <shjang1992@gmail.com>
2016-11-13 15:58:07 +0900

    Tank now moves based on A*

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -88,6 +88,12 @@ public class TankController : MonoBehaviour {
         else {
             Debug.Log("path : " + p.path.Count);
         }
+
+        if (MoveCoroutine != null) {
+            StopCoroutine(MoveCoroutine);
+        }
+
+        MoveCoroutine = StartCoroutine(PointMove(p.vectorPath));
     }
 
     private void MoveTo(Vector3 destPos)
@@ -95,13 +101,6 @@ public class TankController : MonoBehaviour {
         movePos = destPos;
 
         seeker.StartPath(transform.position, destPos, OnPathComplete);
-        return;
-
-        if (MoveCoroutine != null) {
-            StopCoroutine(MoveCoroutine);
-        }
-
-        // MoveCoroutine = StartCoroutine(PointMove(path.Path));
     }
 
     public void Shoot(float power, int bounceCount)
@@ -116,6 +115,7 @@ public class TankController : MonoBehaviour {
         bulletRigid.isKinematic = false;
         bulletRigid.AddRelativeForce(Vector3.up * power);
     }
+
     public void HeadRotate(Vector3 deltaPos)
     {
         deltaPos *= 100;
@@ -128,5 +128,38 @@ public class TankController : MonoBehaviour {
             TankCannon.transform.localRotation = Quaternion.Euler(new Vector3(-25f, 0, 0));
         else if (cannonAngle > 25f)
             TankCannon.transform.localRotation = Quaternion.Euler(new Vector3(25f, 0, 0));
+    }
+
+    public IEnumerator PointMove(List<Vector3> pathList)
+    {
+        foreach (var point in pathList) {
+            float distanceX = point.x - this.transform.position.x;
+            float distanceZ = point.z - this.transform.position.z;
+            float angle = Mathf.Atan2(distanceX, distanceZ) * Mathf.Rad2Deg;
+            Quaternion target = Quaternion.Euler(0, angle, 0);
+
+            while (true) {
+                var vec3rotation = transform.rotation.eulerAngles;
+                var rotation = Quaternion.Euler(0, vec3rotation.y, 0);
+                if (Quaternion.Angle(rotation, target) <= 0.1f) {
+                    break;
+                }
+
+                this.transform.rotation = Quaternion.RotateTowards(transform.rotation, target, 90f * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+
+            while (Vector3.Distance(this.transform.position, point) > 0.01f) {
+                Quaternion rot = Quaternion.Euler(new Vector3(0, angle, 0));
+                transform.rotation = rot;
+
+                var dir = (point - transform.position).normalized;
+                transform.position += dir * speed * Time.deltaTime;
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        MoveCoroutine = null;
     }
 }

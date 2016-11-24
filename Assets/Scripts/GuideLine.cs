8ffe9a9db6de45ce57e909e8cfc3be05ca9c68f8
DaeHyun Kang <limitedcopy@gmail.com>
2016-11-24 12:40:07 +0900

    Piano

---
+++ Assets/Scripts/GuideLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideLine : MonoBehaviour {
+    public GameObject noteLine;
+	// Use this for initialization
+	void Start () {
+        for(float i = ((1f/36f)-0.0045f)*-15; i < ((1f / 36f) - 0.0045f) * 15; i += ((1f / 36f) - 0.0045f))
+        {
+            GameObject newNote = Instantiate(noteLine, noteLine.transform.parent) as GameObject;
+            newNote.transform.localPosition = noteLine.transform.localPosition;
+            newNote.transform.localRotation = noteLine.transform.localRotation;
+            newNote.transform.Translate(new Vector3(i, 0, 0));
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
